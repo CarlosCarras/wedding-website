1e8076af00ada1db80a1d5578f37f5e8246a9e76
Marilyn Braojos <mgutierrez64@gatech.edu>
2026-08-26 09:58:17 -0500

    Add Eduardo + Jessica to gallery

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -120,7 +120,8 @@ const GALLERY_IMAGES_BASE = [
     { name: "Eudorah + Aakash", src: require("../../assets/gallery/aakash_eudorah.webp"), caption: "Atlanta, GA" },
     { name: "Ore", src: require("../../assets/gallery/ore.webp"), caption: "Gainesville, FL" },
     { name: "Ore + Sophie", src: require("../../assets/gallery/ore_sophie.webp"), caption: "The Ocean" },
-    { name: "Jessica + Mikayla", src: require("../../assets/gallery/mikayla_jessica.webp"), caption: "Gainesville, FL" }
+    { name: "Jessica + Mikayla", src: require("../../assets/gallery/mikayla_jessica.webp"), caption: "Gainesville, FL" },
+    { name: "Eduardo + Jessica", src: require("../../assets/gallery/jessica_eduardo.webp"), caption: "Lakeland, FL" }
 ];
 
 const GALLERY_IMAGES = GALLERY_IMAGES_BASE.map((img, i) => ({
